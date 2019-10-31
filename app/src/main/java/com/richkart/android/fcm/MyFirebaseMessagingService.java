195181bf924d8c5fe139a5f4fd7cb8c6a27bfd2d
deepikaAudetemi <deepika.kumawat@audetemi.com>
2019-10-31 21:53:48 +0530

    Push notificaiton from console

--- app/src/main/java/com/richkart/android/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/richkart/android/fcm/MyFirebaseMessagingService.java
@@ -1,15 +1,66 @@
 package com.richkart.android.fcm;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.richkart.android.DrawerActivity;
+import com.richkart.android.R;
+
+import org.json.JSONObject;
+
+import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private final String MESSAGE = "message";
+    private final String ACTION = "action";
+    private Intent intent;
+
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
+        Map data = remoteMessage.getData();
+        JSONObject object = new JSONObject(data);
+        sendNotification(object.optString(MESSAGE, ""), object.optString(ACTION, ""));
     }
 
 
+    private void sendNotification(String messageBody, String action) {
+
+        intent = new Intent(this, DrawerActivity.class);
+        OpenScreen(messageBody);
+
+
+    }
+
+
+    private void OpenScreen(String messageBody) {
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Richkart")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
 }
